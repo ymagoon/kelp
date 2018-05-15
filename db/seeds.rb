1d20562c4a1685084fe3b9e813713ff74b2ed973
Yitzhak Magoon <ymagoon@gmail.com>
2018-05-15 17:53:53 +0800

    Require all scripts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
+require_relative 'add_missing_location'
 require_relative 'populate_dive_training_orgs'
+require_relative 'populate_load_dive_centers'
 require_relative 'populate_sditdi_dive_centers'
+require_relative 'populate_ssi_dive_centers'
+require_relative 'scrape_ssi_dive_centers'
 require_relative 'helper'
 
+# add_missing_location
 # populate_dive_training_orgs
+# populate_load_dive_centers('file')
 # populate_sditdi_dive_centers
 # populate_ssi_dive_centers
-# populate_load_dive_centers('file')
