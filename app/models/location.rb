e0685303f5ce1af687719381e230b740fbc221ff
Yitzhak Magoon <ymagoon@gmail.com>
2018-05-17 05:39:26 +0800

    Add missing detail methods to Location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,38 @@
 class Location < ApplicationRecord
   belongs_to :training_organization, optional: true
   belongs_to :dive_center, optional: true
+
+  def self.missing_details
+    locations = Location.all
+
+    details = locations.select do |loc|
+      loc.address_1 == nil || loc.city == nil || loc.state == nil || loc.country == nil || loc.postal_code == nil
+    end
+
+    details
+  end
+
+  def self.missing_address_1
+    Location.where(address_1: nil)
+  end
+
+  def self.missing_address_2
+    Location.where(address_2: nil)
+  end
+
+  def self.missing_city
+    Location.where(city: nil)
+  end
+
+  def self.missing_state
+    Location.where(state: nil)
+  end
+
+  def self.missing_country
+    Location.where(country: nil)
+  end
+
+  def self.missing_postal_code
+    Location.where(postal_code: nil)
+  end
 end
