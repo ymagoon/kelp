7fe7306ab8c6231d320414be1fc958d54ecdbebd
Yitzhak Magoon <ymagoon@gmail.com>
2018-06-08 12:31:40 +0800

    Update algorithm to scrape all PADI DCs

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,12 @@ require_relative 'scripts/populate_ssi_dive_centers'
 require_relative 'scripts/scrape_ssi_dive_centers'
 require_relative 'scripts/populate_dive_centers'
 require_relative 'scripts/helper'
+require_relative 'scripts/scrape_padi_dive_centers'
 
 # add_missing_location
 # populate_dive_centers
 # populate_training_organizations #
 # populate_load_dive_centers('file') #
 # populate_sditdi_dive_centers #
-populate_ssi_dive_centers
+# populate_ssi_dive_centers
+scrape_padi_dive_centers
