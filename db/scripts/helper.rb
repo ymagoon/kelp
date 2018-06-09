2862b80ce2677c2c1dd5e33ad40174c1192f4a6e
Yitzhak Magoon <ymagoon@gmail.com>
2018-06-09 13:08:37 +0800

    Add padi country names to countries.json, create super basic dive center show page

--- db/scripts/helper.rb
+++ db/scripts/helper.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 EMAIL_EXPRESSION = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 URL_EXPRESSION = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/
 
