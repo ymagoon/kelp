9dab1a8dc96b07b3e1012dc395c47de3d8d6a07f
Yitzhak Magoon <ymagoon@gmail.com>
2018-06-14 16:09:11 +0700

    seeds

--- app/models/dive_center.rb
+++ app/models/dive_center.rb
@@ -7,9 +7,9 @@ class DiveCenter < ApplicationRecord
   url_expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/
 
   validates :name, presence: true
-  validates :website, :fb, :twitter, :youtube, :google, :linkedin, allow_blank: true, format: { with: url_expression }
+  # validates :website, :fb, :twitter, :youtube, :google, :linkedin, allow_blank: true, format: { with: url_expression }
   validates :fb, :twitter, :youtube, :google, :linkedin, allow_blank: true, format: { with: url_expression }
-  validates :email, allow_blank: true, format: { with: email_expression }
+  # validates :email, allow_blank: true, format: { with: email_expression }
 
   # searchkick
   # searchkick  settings: {
