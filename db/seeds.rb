e921695e18167aa6fce4a78ea1bff38dd1f5af3d
Yitzhak Magoon <ymagoon@gmail.com>
2018-06-14 15:38:58 +0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ require_relative 'scripts/populate_padi_dive_centers'
 
 # add_missing_location
 # populate_dive_centers
-# populate_training_organizations #
+populate_training_organizations #
 # populate_load_dive_centers('file') #
-# populate_sditdi_dive_centers #
+populate_sditdi_dive_centers #
 # populate_ssi_dive_centers
 # scrape_padi_dive_centers
-populate_padi_dive_centers
+# populate_padi_dive_centers
