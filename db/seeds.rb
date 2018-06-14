70b8173a1482557181ba2dff9394ead07b219e92
Yitzhak Magoon <ymagoon@gmail.com>
2018-06-14 16:04:54 +0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 # require_relative 'scripts/add_missing_location'
-require_relative 'scripts/populate_training_organizations'
-# require_relative 'scripts/populate_load_dive_centers'
-# require_relative 'scripts/populate_sditdi_dive_centers'
+# require_relative 'scripts/populate_training_organizations'
+require_relative 'scripts/populate_load_dive_centers'
+require_relative 'scripts/populate_sditdi_dive_centers'
 # require_relative 'scripts/populate_ssi_dive_centers'
 # require_relative 'scripts/scrape_ssi_dive_centers'
 # require_relative 'scripts/populate_dive_centers'
 require_relative 'scripts/helper'
 # require_relative 'scripts/scrape_padi_dive_centers'
-# require_relative 'scripts/populate_padi_dive_centers'
+require_relative 'scripts/populate_padi_dive_centers'
 
 # add_missing_location
 # populate_dive_centers
-populate_training_organizations #
-# populate_load_dive_centers('file') #
-# populate_sditdi_dive_centers #
+# populate_training_organizations #
+populate_load_dive_centers('file') #
+populate_sditdi_dive_centers #
 # populate_ssi_dive_centers
 # scrape_padi_dive_centers
-# populate_padi_dive_centers
+populate_padi_dive_centers
