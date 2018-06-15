cd60a4c1a0b83540e88122e2565bacf4766c7fdf
Yitzhak Magoon <ymagoon@gmail.com>
2018-06-15 16:05:37 +0700

    configure react components

--- app/javascript/packs/components/App2.jsx
+++ app/javascript/packs/components/App2.jsx
@@ -1,291 +1,81 @@
 import React, { Component } from 'react';
-import { ReactiveBase, DataSearch, MultiList, SingleRange, RangeSlider } from '@appbaseio/reactivesearch';
-import { MultiDataList, DateRange, ResultCard } from '@appbaseio/reactivesearch';
+import { ReactiveBase,
+         DataSearch,
+         ResultList
+       }
+       from '@appbaseio/reactivesearch';
+// import { MultiDataList, DateRange, ResultCard, TextField } from '@appbaseio/reactivesearch';
 
 class Main extends React.Component {
   render() {
     return (
       <div className="main-container">
         <ReactiveBase
-          app="dive_centers_development_20180614135556568"
-          url="http://localhost:9200"
-         >
+          app="dive_centers_development_20180615145932138"
+          url="http://localhost:9200">
+
           <DataSearch
             componentId="mainSearch"
-            dataField={["original_title.search"]}
+            dataField={["country.search",
+                        "country.raw",
+                        "city.search",
+                        "city.raw",
+                        "state.search",
+                        "state.raw",]}
             className="search-bar"
             queryFormat="and"
-            placeholder="Search for movies..."
-          />
-          <MultiList
-            componentId="genres-list"
-            dataField="genres_data.raw"
-            className="genres-filter"
-            size={20}
-            sortBy="asc"
-            queryFormat="or"
-            selectAllLabel="All Genres"
-            showCheckbox={true}
-            showCount={true}
-            showSearch={true}
-            placeholder="Search for a Genre"
-            react={{
-                and: [
-                    "mainSearch",
-                    "results",
-                    "date-filter",
-                    "RangeSlider",
-                    "language-list",
-                    "revenue-list"
-                ]
-            }}
-            showFilter={true}
-            filterLabel="Genre"
-            URLParams={false}
-            innerClass={{
-                label: "list-item",
-                input: "list-input"
-            }}
+            placeholder="Discover your next dive destination..."
+            fuzziness={1} // when a user types
+            defaultSuggestions={[{label: "Philippines", value: "Philippines"},
+                                 {label: "Indonesia", value: "Indonesia"},
+                                 {label: "Mexico", value: "Mexico"},
+                                 {label: "Bahamas", value: "Bahamas"}]}
+            autosuggest={true}
+            showClear={true}
           />
 
-          <SingleRange
-              componentId= "revenue-list"
-              dataField= "ran_revenue"
-              className= "revenue-filter"
-              data={[
-                { start: 0, end: 1000, label: "< 1M" },
-                { start: 1000, end: 10000, label: "1M-10M" },
-                { start: 10000, end: 500000, label: "10M-500M" },
-                { start: 500000, end: 1000000, label: "500M-1B" },
-                { start: 1000000, end: 10000000, label: "> 1B" }
-              ]}
-              showRadio={true}
-              showFilter={true}
-              filterLabel="Revenue"
-              URLParams={false}
-              innerClass= {{
-                label: "revenue-label",
-                radio: "revenue-radio"
-              }}
-          />
-          <RangeSlider
-            componentId="RangeSlider"
-            dataField="vote_average"
-            className="review-filter"
-            title="ratings"
-            range={{
-              start: 0,
-              end: 10
-            }}
-            rangeLabels={{
-              start: "0",
-              end: "10"
-            }}
-            react={{
-              and: [
-                "mainSearch",
-                "results",
-                "language-list",
-                "date-Filter",
-                "genres-list",
-                "revenue-list"
-              ]
-            }}
-          />
-          <MultiDataList
-            componentId="language-list"
-            dataField="original_language.raw"
-            className="language-filter"
-            title="language"
-            size={100}
-            sortBy="asc"
-            queryFormat="or"
-            selectAllLabel="All Languages"
-            showCheckbox={true}
-            showSearch={true}
-            placeholder="Search for a language"
-            react={{
-              and: [
-                "mainSearch",
-                "results",
-                "date-filter",
-                "RangeSlider",
-                "genres-list",
-                "revenue-list"
-              ]
-            }}
-            data={[
-              {
-                label: "English",
-                value: "English"
-              },
-              {
-                label: "Chinese",
-                value: "Chinese"
-              },
-              {
-                label: "Hindi",
-                value: "Hindi"
-              }
-            ]}
-            showFilter={true}
-            filterLabel="Language"
-            URLParams={false}
-            innerClass={{
-              label: "list-item",
-              input: "list-input"
-            }}
-          />
-          <DateRange
-            componentId="date-filter"
-            dataField="release_date"
-            className="datePicker"
-          />
-          <ResultCard
-            componentId="results"
-            dataField="original_title"
-            pagination={true}
-            paginationAt="bottom"
-            pages={5}
-            size={12}
+          <ResultList
+            componentId="searchResult"
+            dataField={"country"}
+            size={10}
+            className="result-list-container"
+            noResults="No dive centers found..."
             Loader="Loading..."
-            showResultStats={true}
-            noResults="No results were found..."
-            sortOptions={[
-              {
-                dataField: "revenue",
-                sortBy: "desc",
-                label: "Sort by Revenue(High to Low)"
-              },
-              {
-                dataField: "popularity",
-                sortBy: "desc",
-                label: "Sort by Popularity(High to Low)"
-              },
-              {
-                dataField: "vote_average",
-                sortBy: "desc",
-                label: "Sort by Ratings(High to Low)"
-              },
-              {
-                dataField: "original_title.raw",
-                sortBy: "asc",
-                label: "Sort by Title(A-Z)"
-              }
-            ]}
-            className="Result_card"
-            innerClass={{
-              title: "result-title",
-              listItem: "result-item",
-              list: "list-container",
-              sortOptions: "sort-options",
-              resultStats: "result-stats",
-              resultsInfo: "result-list-info",
-              poweredBy: "powered-by"
-            }}
             react={{
-              and: [
-                "mainSearch",
-                "RangeSlider",
-                "language-list",
-                "date-filter",
-                "genres-list",
-                "revenue-list"
-              ]
+              and: 'mainSearch',
             }}
-            onData={function(res) {
-              return {
-                description: (
-                  <div className="main-description">
-                    <div className="ih-item square effect6 top_to_bottom">
-                      <a
-                        target="#"
-                        href={"http://www.imdb.com/title/" + res.imdb_id}
-                      >
-                        <div className="img">
-                          <img
-                            src={
-                              "https://image.tmdb.org/t/p/w500" +
-                              res.poster_path
-                            }
-                            alt={res.original_title}
-                            className="result-image"
-                          />
-                        </div>
-                        <div className="info colored">
-                          <h3 className="overlay-title">
-                            {res.original_title}
-                          </h3>
-
-                          <div className="overlay-description">
-                            {res.tagline}
-                          </div>
-
-                          <div className="overlay-info">
-                            <div className="rating-time-score-container">
-                              <div className="sub-title Rating-data">
-                                <b>
-                                  Imdb
-                                  <span className="details">
-                                    {" "}
-                                    {res.vote_average}/10{" "}
-                                  </span>
-                                </b>
-                              </div>
-                              <div className="time-data">
-                                <b>
-                                  <span className="time">
-                                    <i className="fa fa-clock-o" />{" "}
-                                  </span>{" "}
-                                  <span className="details">
-                                    {res.time_str}
-                                  </span>
-                                </b>
-                              </div>
-                              <div className="sub-title Score-data">
-                                <b>
-                                  Score:
-                                  <span className="details">
-                                    {" "}
-                                    {res.score}
-                                  </span>
-                                </b>
-                              </div>
-                            </div>
-                            <div className="revenue-lang-container">
-                              <div className="revenue-data">
-                                <b>
-                                  <span>Revenue:</span>{" "}
-                                  <span className="details">
-                                    {" "}
-                                    ${res.or_revenue}
-                                  </span>{" "}
-                                </b>
-                              </div>
-
-                              <div className="sub-title language-data">
-                                <b>
-                                  Language:
-                                  <span className="details">
-                                    {" "}
-                                    {res.original_language}
-                                  </span>
-                                </b>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                ),
-                url: "http://www.imdb.com/title/" + res.imdb_id
-              };
-            }}
+            onData={this.dcList}
           />
         </ReactiveBase>
       </div>
     );
+  }
+  dcList(data) {
+    // console.log(data);
+    return {
+      title: <div className="dive-center-name" dangerouslySetInnerHTML={{ __html: data.name }} />,
+      description: (
+        <div className="flex column justify-space-between">
+          <div>
+            <div><span className="email">http://{data.website}</span></div>
+            <div><span className="email">{data.email}</span></div>
+            <div><span className="fb">{data.fb}</span> </div>
+            <div>city: <span className="fb">{data.city}</span> </div>
+            <div>state:<span className="fb">{data.state}</span> </div>
+            <div>country:<span className="fb">{data.country}</span> </div>
+            <div className="ratings-list flex align-center">
+             {/* <span className="stars">
+                {
+                  Array(data.average_rating_rounded).fill('x')
+                    .map((item, index) => <i className="fas fa-star" key={index} />) // eslint-disable-line
+                }
+              </span>*/}
+              <span className="phone">{data.phone_number}</span>
+            </div>
+          </div>
+        </div>
+      )
+    };
   }
 }
 export default Main;
--- app/models/dive_center.rb
+++ app/models/dive_center.rb
@@ -7,145 +7,157 @@ class DiveCenter < ApplicationRecord
   url_expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/
 
   validates :name, presence: true
-  # validates :website, :fb, :twitter, :youtube, :google, :linkedin, allow_blank: true, format: { with: url_expression }
+  validates :website, :fb, :twitter, :youtube, :google, :linkedin, allow_blank: true, format: { with: url_expression }
   validates :fb, :twitter, :youtube, :google, :linkedin, allow_blank: true, format: { with: url_expression }
-  # validates :email, allow_blank: true, format: { with: email_expression }
+  validates :email, allow_blank: true, format: { with: email_expression }
 
   # searchkick
-  # searchkick  settings: {
-  #               analysis: {
-  #                  filter: {
-  #                     nGram_filter: {
-  #                        type: "nGram",
-  #                        min_gram: 2,
-  #                        max_gram: 20,
-  #                        token_chars: [
-  #                           "letter",
-  #                           "digit",
-  #                           "punctuation",
-  #                           "symbol"
-  #                        ]
-  #                     }
-  #                  },
-  #                  analyzer: {
-  #                     nGram_analyzer: {
-  #                        type: "custom",
-  #                        tokenizer: "whitespace",
-  #                        filter: [
-  #                           "lowercase",
-  #                           "asciifolding",
-  #                           "nGram_filter"
-  #                        ]
-  #                     },
-  #                     whitespace_analyzer: {
-  #                        type: "custom",
-  #                        tokenizer: "whitespace",
-  #                        filter: [
-  #                           "lowercase",
-  #                           "asciifolding"
-  #                        ]
-  #                     }
-  #                  }
-  #               }
-  #            },
+  searchkick  settings: {
+                analysis: {
+                   filter: {
+                      nGram_filter: {
+                         type: "nGram",
+                         min_gram: 2,
+                         max_gram: 20,
+                         token_chars: [
+                            "letter",
+                            "digit",
+                            "punctuation",
+                            "symbol"
+                         ]
+                      }
+                   },
+                   analyzer: {
+                      ngram_analyzer: {
+                         type: "custom",
+                         tokenizer: "whitespace",
+                         filter: [
+                            "lowercase",
+                            "asciifolding",
+                            "nGram_filter"
+                         ]
+                      }
+                      # whitespace_analyzer: {
+                      #    type: "custom",
+                      #    tokenizer: "whitespace",
+                      #    filter: [
+                      #       "lowercase",
+                      #       "asciifolding"
+                      #    ]
+                      # }
+                   }
+                }
+             },
              # merge_mappings: true,
-  #            mappings: {
-  #              dive_center: {
-  #                 dynamic_templates:[
-  #                    {
-  #                       string_template: {
-  #                          match: "*",
-  #                          match_mapping_type: "string",
-  #                          mapping: {
-  #                             "fields": {
-  #                             },
-  #                             ignore_above: 30000,
-  #                             type: "keyword"
-  #                          }
-  #                       }
-  #                    }
-  #                 ],
-  #                 properties: {
-  #                    bar: {
-  #                       "type": "boolean"
-  #                    },
-  #                    city: {
-  #                       type: "keyword",
-  #                       fields: {
-  #                          analyzed: {
-  #                             type: "text",
-  #                             analyzer: "nGram_analyzer",
-  #                             search_analyzer: "whitespace_analyzer"
-  #                          }
-  #                       },
-  #                       ignore_above: 30000
-  #                    },
-  #                    country: {
-  #                       type: "keyword",
-  #                       fields: {
-  #                          analyzed: {
-  #                             type: "text",
-  #                             analyzer: "nGram_analyzer",
-  #                             search_analyzer: "whitespace_analyzer"
-  #                          }
-  #                       },
-  #                       ignore_above: 30000
-  #                    },
-  #                    dive_center_type: {
-  #                       type: "keyword",
-  #                       ignore_above: 30000
-  #                    },
-  #                    lodging: {
-  #                       type: "boolean"
-  #                    },
-  #                    name: {
-  #                       type: "keyword",
-  #                       fields: {
-  #                          analyzed: {
-  #                             type: "text",
-  #                             analyzer: "nGram_analyzer",
-  #                             search_analyzer: "whitespace_analyzer"
-  #                          }
-  #                       },
-  #                       ignore_above: 30000
-  #                    },
-  #                    nitrox: {
-  #                       type: "boolean"
-  #                    },
-  #                    pool: {
-  #                       type: "boolean"
-  #                    },
-  #                    rent_equipment: {
-  #                       type: "boolean"
-  #                    },
-  #                    restaurant: {
-  #                       type: "boolean"
-  #                    },
-  #                    state: {
-  #                       type: "keyword",
-  #                       fields: {
-  #                          analyzed: {
-  #                             type: "text",
-  #                             analyzer: "nGram_analyzer",
-  #                             search_analyzer: "whitespace_analyzer"
-  #                          }
-  #                       },
-  #                       ignore_above: 30000
-  #                    },
-  #                    transfers: {
-  #                      type: "boolean"
-  #                    }
-  #                 }
-  #              }
-  #           }
+             mappings: {
+               dive_center: {
+                  dynamic_templates:[
+                     {
+                        string_template: {
+                           match: "*",
+                           match_mapping_type: "string",
+                           mapping: {
+                              "fields": {
+                              },
+                              ignore_above: 30000,
+                              type: "keyword"
+                           }
+                        }
+                     }
+                  ],
+                  properties: {
+                     bar: {
+                        type: "boolean"
+                     },
+                     city: {
+                       type: "text",
+                       fields: {
+                         raw: {
+                           type: "keyword"
+                         },
+                         search: {
+                           type: "text",
+                           analyzer: "ngram_analyzer",
+                           search_analyzer: "simple"
+                         }
+                       },
+                       analyzer: "standard"
+                     },
+                     country: {
+                       type: "text",
+                       fields: {
+                         raw: {
+                           type: "keyword"
+                         },
+                         search: {
+                           type: "text",
+                           analyzer: "ngram_analyzer",
+                           search_analyzer: "simple"
+                         }
+                       },
+                       analyzer: "standard"
+                     },
+                     dive_center_type: {
+                        type: "keyword",
+                        ignore_above: 30000
+                     },
+                     lodging: {
+                        type: "boolean"
+                     },
+                     name: {
+                       type: "text",
+                       fields: {
+                         raw: {
+                           type: "keyword"
+                         },
+                         search: {
+                           type: "text",
+                           analyzer: "ngram_analyzer",
+                           search_analyzer: "simple"
+                         }
+                       },
+                       analyzer: "standard"
+                     },
+                     nitrox: {
+                        type: "boolean"
+                     },
+                     pool: {
+                        type: "boolean"
+                     },
+                     rent_equipment: {
+                        type: "boolean"
+                     },
+                     restaurant: {
+                        type: "boolean"
+                     },
+                     state: {
+                       type: "text",
+                       fields: {
+                         raw: {
+                           type: "keyword"
+                         },
+                         search: {
+                           type: "text",
+                           analyzer: "ngram_analyzer",
+                           search_analyzer: "simple"
+                         }
+                       },
+                       analyzer: "standard"
+                     },
+                     transfers: {
+                       type: "boolean"
+                     }
+                  }
+               }
+            }
 
-  # def search_data
-  #   attributes.merge(
-  #     city: location.city,
-  #     state: location.state,
-  #     country: location.country
-  #   )
-  # end
+  def search_data
+    attributes.merge(
+      city: location.city,
+      state: location.state,
+      country: location.country
+    )
+  end
 
   # def search_data
   #   {
